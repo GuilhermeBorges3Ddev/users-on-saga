445c54480b28bee4d65a26966fe3295facfc890c
guilhermeborges-bb <guilherme.borges@brandbastion.com>
2026-01-13 08:30:28 -0300

    Redux with basic actions plugged over all app

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import axios from 'axios'
 import ReactDOM from 'react-dom'
 
+import { createStore } from 'redux'
+import { Provider} from 'react-redux'
+import reducers from './reducers/index.js'
+
 import App from './components/App.jsx'
 
 axios.defaults.baseURL = 'http://localhost:3004/'
-//axios.get('/users')
+
+const store = createStore(reducers)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
